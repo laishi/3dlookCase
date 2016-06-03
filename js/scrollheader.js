49cf9c1e8412d72ecc31dbefcf136263c23589f4
laishi <laishi886@hotmail.com>
2016-06-03 20:39:51 +0800

    基础框架搭建完成

--- js/scrollheader.js
+++ js/scrollheader.js
@@ -11,18 +11,43 @@ $(document).ready(function() {
             TweenMax.to($("header"), 0.3, {height: 60,}, 0.1);
 
             TweenMax.to($("header").find("img"), 0.3, { scale: 0 } );
-            TweenMax.to($("header").find(".tagName"), 0.3, { y: -260 } );
+            TweenMax.to($("header").find(".tagTitle"), 0.3, { y: -260 } );
 
         } else {
 
             TweenMax.to($("header").find("img"), 0.3, { scale: 1 } );
 
             TweenMax.to($("header"), 0.3, { height: 320, }, 0.1);
-            TweenMax.to($("header").find(".tagName"), 0.3, { y: 0 } );
+            TweenMax.to($("header").find(".tagTitle"), 0.3, { y: 0 } );
         }
 
 
-        // var detailTitleT = $(".detailTitle h3").offset().top - $(window).scrollTop();
+        var detailTitleT = $(".detailTitle").offset().top - $(window).scrollTop();
+
+        if (detailTitleT < 0) {
+
+            var title = $.trim($(".detailTitle").html());
+            var tag   = $("header").find(".tagTip").html();
+
+
+            $(".titlePos").html(title);
+
+            TweenMax.to($("header").find(".tagTitle"), 0.3, { y: -320 } );
+            // TweenMax.to($(".titlePos"), 0.3, { y: -320 } );
+
+            
+
+
+            console.log(title)
+            
+        } else {
+
+
+
+        }
+
+
+
 
 
         // if ($("header").hasClass("fixHeader")) {
@@ -39,8 +64,6 @@ $(document).ready(function() {
         //         $("header").find(".tagName").children().remove();
         //         $("header").find(".tagName").append(tag);                
         //     }
-
-
 
         // }
 
