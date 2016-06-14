8adf8109cbd546ae7970d1dfd0d1e1570b256742
laishi <laishi886@hotmail.com>
2016-06-14 11:53:25 +0800

    clear

--- incude/vue/vueItem.js
+++ incude/vue/vueItem.js
@@ -5,32 +5,6 @@ var HtmlFormat = ".html"
 
 
 
-
-//GET FLODER NAME
-
-// var workNames = [];
-
-// var workTitleImg = "img/workImg/titleImg";
-// var fileextension = ".png";
-// $.ajax({
-//     url: workTitleImg,
-//     async: false,
-//     success: function (data) {
-//         $(data).find("a:contains(" + fileextension + ")").each(function (index,item) {            
-//             var CurURL = this.href.replace(window.location.host, "").replace("http:///", "");
-
-//             var urlSplit   = CurURL.split("/");
-//             var urlLenght  = urlSplit.length;
-//             var curUrlName = urlSplit[urlLenght-1].replace(/\.png/, '');
-//             workNames.push(curUrlName);
-//         });
-//     }
-// });
-
-
-
-
-
 //  WORK
 
 var db = new loki('workDB.json');
@@ -90,7 +64,7 @@ var db = new loki('blogDB.json');
 var blogCol = db.addCollection('blogCol');
 
 
-var blogNames = ["waveLoader", "playcanvas-pbr", "blend4web", "playCanvas", "threeJS", "webglFramework", "pbr", "lokiJS", ];
+var blogNames = ["waveLoader", "playcanvas-introduce", "lokiJS", ];
 var blogHtmlFolder = "pages/blog/";
 var blogImgFolder = "img/blogImg/";
 
@@ -200,6 +174,7 @@ var lookVue = new Vue({
 });
 
 
+console.log(lookVue.work[0].title)
 
 
 
@@ -209,29 +184,148 @@ var lookVue = new Vue({
 
 
 
+var notFound = Vue.extend({
+   template: '<h1>Not Found</h1>' +
+   '<router-view></router-view>'
+})
+
+var workComponent = Vue.extend({
+    template: 
+            '<h1>Work</h1>' +
+            '<router-view></router-view>'
+})
+
+var blogComponent = Vue.extend({
+   template: 
+            '<h1>Blog</h1>' +
+            '<router-view></router-view>'
+})
+
+var aboutComponent = Vue.extend({
+   template: 
+            '<h1> aboutComponent </h1>' +
+            '<a v-link="{ path: \'/subroute\' }" class="btn btn-lg btn-primary" role="button">View SubRoute</a>' +
+            '<router-view></router-view>'
+})
+
+var contactComponent = Vue.extend({
+   template: 
+            '<h1> 联系 </h1>' +
+            '<router-view></router-view>'
+})
+
+
+
+var detail = Vue.extend({
+   template: 
+               '<h1>Navbar example</h1>' +
+               '<p>This example is a quick exercise to illustrate how the default, static and fixed to top navbar work. It includes the responsive CSS and HTML, so it also adapts to your viewport and device.</p>' +
+               '<p>To see the difference between static and fixed top navbars, just scroll.</p>'
+})
+
+
+
+
+Vue.config.debug = true;
+
+Vue.use(VueRouter)
+
+var router = new VueRouter({
+   history: false,
+   hashbang:true,
+   root: '/'
+})
 
 
 
 
 
-// window.addEventListener('scroll', updateItem, false);
+
+Vue.component('newtemp', {
+    template: '#workVUE',
+     route: {
+       data: function(transition) {
+            transition.next({
+                // saving the id which is passed in url to $data
+                itemName: transition.to.params.itemName
+            });
+        }
+     },
+    data: function() {
+        return {
+            itemName:itemName,
+          }
+       },
+})
 
 
-// function updateItem() {
 
 
 
-//     var winTop = $(window).scrollTop();
+router.map({
+   // '*': {
+   //    component: notFound
+   // },
 
-//     var limitNum = winTop / 50 ;
+   '/': {
+      component: workComponent,
+      
+      subRoutes: {
+      '/detail': {
+           component: detail
+         }
+      }
+   },
+
+   '/work': {
+      component: workComponent,
+      
+      subRoutes: {
+      '/detail': {
+           component: detail
+         }
+      }
+   },
+
+   '/blog': {
+      component: blogComponent,
+      
+      subRoutes: {
+      '/detail': {
+           component: detail
+         }
+      }
+   },
+
+   '/about': {
+      component: aboutComponent,
+      subRoutes: {
+      '/detail': {
+           component: detail
+         }
+      }
+   },
+
+   '/contact': {
+      component: contactComponent
+   },
+
+
+  '/detail/:itemName': {
+    name: 'detail', // 给这条路径加上一个名字
+    component: Vue.component('newtemp')
+  },
+
+
+   '/admin': {
+      component: contactComponent
+   },
+});
 
 
 
-//     // var itemChain = blogCol.chain();
-//     // var blogItem = itemChain.offset(limitNum).limit(8);
 
 
-// };
+var App = Vue.extend()
 
-
-
+router.start(App, 'body')
--- incude/vue/vueconfig.js
+++ incude/vue/vueconfig.js
@@ -49,6 +49,35 @@ var router = new VueRouter({
    root: '/'
 })
 
+
+
+
+
+Vue.component('newtemp', {
+    template: '#newtemp',
+     route: {
+       data: function(transition) {
+            transition.next({
+                // saving the id which is passed in url to $data
+                name: transition.to.params.name
+            });
+        }
+     },
+    data: function() {
+        return {
+            name:name,
+          }
+       },
+})
+
+
+
+
+
+
+
+
+
 router.map({
    // '*': {
    //    component: notFound
@@ -107,13 +136,94 @@ router.map({
    '/admin': {
       component: contactComponent
    },
-
-
-
-
-
 });
+
+
+
+
 
 var App = Vue.extend()
 
 router.start(App, 'body')
+
+
+
+
+
+
+
+
+
+
+////////////////////////////////////////////////////////////////////////////////////////
+
+
+
+
+
+Vue.component('app-page', {
+    template: '#userBlock',
+
+    data: function() {
+      return{
+        
+        users: []
+      }
+    },
+
+    ready: function () {
+      this.fetchUsers();
+    },
+
+    methods: {
+      fetchUsers: function(){
+        var users = [
+          {
+            id: 1,
+            name: 'tom'
+          },
+          {
+            id: 2,
+            name: 'brian'
+          },
+          {
+            id: 3,
+            name: 'sam'
+          },
+        ];
+
+      this.$set('users', users);
+      }
+    }
+})
+
+Vue.component('newtemp', {
+    template: '#newtemp',
+     route: {
+       data: function(transition) {
+            transition.next({
+                // saving the id which is passed in url to $data
+                name: transition.to.params.name
+            });
+        }
+     },
+    data: function() {
+        return {
+            name:name,
+          }
+       },
+})
+
+
+router.map({
+    '/': {
+        component: Vue.component('app-page')
+        },
+    'new/:name': {
+        name: 'new',
+        component: Vue.component('newtemp')
+    },
+  
+})
+
+router.start(App, '#page-content')
